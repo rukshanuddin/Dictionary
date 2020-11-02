650e8a11888e83b5506408841d770ba31e21baf4
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-11-01 21:40:55 -0500

    created menu links

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,11 +1,7 @@
 # Helper modules for Navbar
 module NavigationHelper
   def collapsible_links_partial_path
-    if user_signed_in?
-      'layouts/navigation/collapsible_elements/signed_in_links'
-    else
       'layouts/navigation/collapsible_elements/non_signed_in_links'
-    end
   end
 
   def nav_header_content_partials

5f09b4f72e50c2686bad95bfd6cb689a9932bdf0
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-11-06 19:59:28 -0500

    moved dictionary logic dictionary.rb+ fix params

--- app/controllers/word_controller.rb
+++ app/controllers/word_controller.rb
@@ -1,8 +1,3 @@
-require "uri"
-require "net/http"
-require "json"
-
-EN_US_ENDPOINT_URL = "https://od-api.oxforddictionaries.com/api/v2/entries/en-us/"
 
 class WordController < ApplicationController
 
@@ -18,41 +13,18 @@ class WordController < ApplicationController
     if @word
       redirect_to action: "show", id: @word.id
     else
-      query = params[:q].chomp
-      begin
-        url = URI("https://od-api.oxforddictionaries.com/api/v2/entries/en-us/#{query}")
-        https = Net::HTTP.new(url.host, url.port)
-        https.use_ssl = true
-
-        request = Net::HTTP::Get.new(url)
-        request["app_id"] = ENV['APP_ID']
-        request["app_key"] = ENV['APP_KEY']
-
-        response = https.request(request)
-        result = JSON.parse(response.read_body)
-        @word = Word.create(name: result["id"]) do |word|
-          word.definition = result["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0]
-        end
-        redirect_to action: "show", id: @word.id
-      rescue URI::InvalidURIError => e
-        logger.error("Error: #{e.exception}")
-        flash[:notice] = "#{query} is not a valid search term, remove any spaces and try again"
-        redirect_to error_path
-      rescue Exception => e
-        puts "ERROR: An error occured, #{response.code} HTTP code received."
-        puts "#{response.error_type}. ERROR: #{e.exception}\n"
-        puts "Error: #{response.header.read_body}"
-        logger.error("#{response.code} #{response.msg}")
-        logger.error(e)
-        logger.error(response.to_hash)
-        flash[:notice] = "#{response.header.read_body}"
-        redirect_to error_path
-      end
+      Dictionary.call(word_params)
     end
   end
 
   def show
     @word = Word.find_by(id: params[:id])
+  end
+
+private
+
+  def word_params
+    params.permit(:endpoint, :q)
   end
 
 end
